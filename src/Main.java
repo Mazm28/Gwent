05969f770467ec0415b939aed37fe35c45722152
Mobin Nakhjavani <mobin.ravoon@gmail.com>
2024-05-03 10:10:01 +0330

    adding some models

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import view.RegisterMenu;
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] argv){
+        Scanner scanner = new Scanner(System.in);
+        RegisterMenu.run(scanner);
+    }
+}
---
+++ src/model/Player.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Player extends User{
+    private ArrayList<Card> deck;
+    private ArrayList<Card> hand;
+}
