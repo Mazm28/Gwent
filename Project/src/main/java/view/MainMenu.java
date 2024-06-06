849ceb4c21692640ce65a3bcb27e48b08b6e7153
Mohammad Bani Ahmadi <mohammad.baniahmadi05@gmail.com>
2024-06-06 16:32:55 +0330

    adding main menu

--- Project/src/main/java/view/MainMenu.java
+++ Project/src/main/java/view/MainMenu.java
@@ -1,14 +1,48 @@
 package view;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
+import model.User;
+
+import java.io.IOException;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class MainMenu{
-    public static void run(Scanner scanner){
-        
+    public Button newGameButton;
+    public Button profileMenuButton;
+    public Button logoutButton;
+
+    public void newGame(MouseEvent mouseEvent) {
+        try {
+            Launcher.changeScene("/FXML/PreGameMenu.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static Matcher getCommandMatcher(String command, String regex){
-        return null;
+    public void logout(MouseEvent mouseEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText("do you want to logout?");
+        Optional<ButtonType> buttonType = alert.showAndWait();
+        if (buttonType.isPresent() && buttonType.get().equals(ButtonType.YES)) {
+            User.setLoggedInUser(null);
+            try {
+                Launcher.changeScene("/FXML/LoginMenu.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void profileMenu(MouseEvent mouseEvent) {
+        try {
+            Launcher.changeScene("/FXML/ProfileMenu.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- README.md
+++
@@ -1,5 +0,0 @@
-# Group-05
-GWENT-project AP
-Mohammad Ali Meschi : 402106491
-Mohammad BaniAhmadi : 402105157
-Mobin RavanNakhjavani : 402106061
