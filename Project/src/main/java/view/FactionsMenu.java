1a08f92189e34d9f03563e2c4edac756a8a903fa
Mohammad Bani Ahmadi <mohammad.baniahmadi05@gmail.com>
2024-06-26 20:51:11 +0330

    adding toolbar

--- Project/src/main/java/view/FactionsMenu.java
+++ Project/src/main/java/view/FactionsMenu.java
@@ -1,16 +1,37 @@
 package view;
 
+import Enums.FXMLAddresses;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import model.PreGame;
 import Enums.Factions;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class FactionsMenu {
-    public Label notify;
-    public Button back;
+public class FactionsMenu implements Initializable {
+    @FXML
+    private Label notify;
+    @FXML
+    private Button back;
+    @FXML
+    private ImageView monsters;
+    @FXML
+    private ImageView scoiatael;
+    @FXML
+    private ImageView skellige;
+    @FXML
+    private ImageView syndicate;
+    @FXML
+    private ImageView northernRealms;
+    @FXML
+    private ImageView nilfgaard;
 
     public void monsters(MouseEvent mouseEvent) {
         notify.setText("You chose \"Monsters\" as your faction");
@@ -63,9 +84,25 @@ public class FactionsMenu {
 
     public void back(MouseEvent mouseEvent) {
         try {
-            Launcher.changeScene("/FXML/PreGameMenu.fxml");
+            Launcher.changeScene(FXMLAddresses.PRE_GAME_MENU.getAddress());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Tooltip monstersTooltip = new Tooltip("Monsters");
+        Tooltip scoiataelTooltip = new Tooltip("Scoia'tael");
+        Tooltip skelligeTooltip = new Tooltip("Skellige");
+        Tooltip syndicateTooltip = new Tooltip("Syndicate");
+        Tooltip northernRealmsTooltip = new Tooltip("Northern Realms");
+        Tooltip nilfgaardTooltip = new Tooltip("Nilfgaard");
+        Tooltip.install(monsters, monstersTooltip);
+        Tooltip.install(scoiatael, scoiataelTooltip);
+        Tooltip.install(skellige, skelligeTooltip);
+        Tooltip.install(syndicate , syndicateTooltip);
+        Tooltip.install(northernRealms, northernRealmsTooltip);
+        Tooltip.install(nilfgaard, nilfgaardTooltip);
     }
 }
