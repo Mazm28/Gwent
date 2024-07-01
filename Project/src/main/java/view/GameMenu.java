1c09687b490020b86cab93834645d6e45fb43c2b
MoAli <meschi.ma24@gmail.com>
2024-07-01 18:09:51 +0330

    wip

--- Project/src/main/java/view/GameMenu.java
+++ Project/src/main/java/view/GameMenu.java
@@ -1,6 +1,29 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 public class GameMenu {
+    @FXML private Label eSiegePowerLabel;
+    @FXML private Label tSiegePowerLabel;
+    @FXML private Label eRangedPowerLabel;
+    @FXML private Label tRangedPowerLabel;
+    @FXML private Label eClosePowerLabel;
+    @FXML private Label tClosePowerLabel;
+    @FXML private Label eTotalPowerLabel;
+    @FXML private Label tTotalPowerLabel;
+    @FXML private ImageView eLeaderImageView;
+    @FXML private ImageView tLeaderImageView;
+    @FXML private HBox mainTableHBox;
+    @FXML private HBox tSiegeHBox;
+    @FXML private HBox tRangedHBox;
+    @FXML private HBox tCloseHBox;
+    @FXML private HBox eSiegeHBox;
+    @FXML private HBox eRangedHBox;
+    @FXML private HBox eCloseHBox;
+    @FXML private HBox specialCardHBox;
     public void initialize() {
 
     }
